f2aad04d1a17846853a2c3b24c060a4d9f98f128
Gaurav775-Git <gaurav775.official@gmail.com>
2026-02-01 11:05:57 +0530

    setup completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-[#222] text-white'>Hi i am Gaurav</div>
+  )
+}
+
+export default App
